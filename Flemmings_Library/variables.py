0529283af42ef7d6ca4885fcf74f133bde95d256
FlemmingR <146717357+Flemming-R@users.noreply.github.com>
2025-02-09 11:57:22 +0100

    Erfolgreich umgestellt auf 'langchain.prompts ChatPromptTemplate

--- Flemmings_Library/variables.py
+++ Flemmings_Library/variables.py
@@ -27,3 +27,24 @@ SQL Query:
 """.strip()
 )
 
+import langchain.prompts
+
+from langchain.prompts import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+chat_prompt_template_sql = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template(
+        "You are a helpful SQL assistant that provides only SELECT-Statements. "
+        "Based on the following database schema, translate a natural language query "
+        "into an SQL query that is executable in my {db_dialect}-Database.\n\n"
+        "Database Schema:\n{schema_info}"
+    ),
+    HumanMessagePromptTemplate.from_template(
+        "Using strictly the schema information above, please formulate the appropriate SQL query for the following question:\n"
+        "Question: {question}\n\nSQL Query:"
+    )
+])
+
